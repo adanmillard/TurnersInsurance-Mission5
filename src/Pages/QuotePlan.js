82e84737323520fb53982de17e55c4acec16beeb
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2022-09-05 16:17:38 +1200

    Stablishing page structure

---
+++ src/Pages/QuotePlan.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from '../components/navbar/NavBar'
+
+function QuotePlan() {
+  
+  
+    return (
+    <div>
+      <NavBar />
+      {/* Component: 4 dots - Personal Details, Vehicle Details, Additional Drivers, Compare & Choose */}
+      <h2>Compare & Choose</h2>
+      
+    </div>
+  )
+}
+
+export default QuotePlan
