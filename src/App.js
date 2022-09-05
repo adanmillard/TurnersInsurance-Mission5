03ae8db967b917617720b1f61fb2fedc8fafdcb0
Adan Millard <adanmillard@gmail.com>
2022-09-06 11:10:18 +1200

    first part of the personal details form completed with no styling or functionality, have made the form as a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { InsuranceHome } from "./Pages/InsuranceHome";
+import { QuotePersonalDetails } from "./Pages/QuotePersonalDetails";
 
 function App() {
   return (
     <div className="App">
       <InsuranceHome />
+      <QuotePersonalDetails />
     </div>
   );
 }
---
+++ src/Pages/QuotePersonalDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavBar } from "../components/navbar/NavBar";
+import { QuotePersonalDetailsOne } from "../components/quotePersonDetailsOne/QuotePersonalDetailsOne";
+
+export const QuotePersonalDetails = () => {
+  return (
+    <div>
+      <NavBar />
+      <QuotePersonalDetailsOne />
+    </div>
+  );
+};
