3501b396cdc899739fdb22b6540c74745e510caa
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2022-09-05 16:32:12 +1200

    Quote Plan structured, footer added to the list

--- src/Pages/QuotePlan.js
+++ src/Pages/QuotePlan.js
@@ -9,7 +9,10 @@ function QuotePlan() {
       <NavBar />
       {/* Component: 4 dots - Personal Details, Vehicle Details, Additional Drivers, Compare & Choose */}
       <h2>Compare & Choose</h2>
-      
+      {/* Body: three options + description on the right */}
+      {/* Component: Add mech insuranse Yes/No */}
+      {/* Component: BACK or Proceed With Quote */}
+      {/* Footer: gray footer from login page */}
     </div>
   )
 }
