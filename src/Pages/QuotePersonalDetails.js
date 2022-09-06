22f9f88e9b76137786b0f2ae65c140b7d8c30022
Adan Millard <adanmillard@gmail.com>
2022-09-06 12:01:43 +1200

    saving changes

--- src/Pages/QuotePersonalDetails.js
+++ src/Pages/QuotePersonalDetails.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavBar } from "../components/navbar/NavBar";
-import { QuotePersonalDetailsOne } from "../components/quotePersonDetailsOne/QuotePersonalDetailsOne";
+// import { QuotePersonalDetailsOne } from "../components/quotePersonDetailsOne/QuotePersonalDetailsOne";
+import { VechicleDetails } from "../components/quoteVehicleDetails/VechicleDetails";
 
 export const QuotePersonalDetails = () => {
   return (
     <div>
       <NavBar />
-      <QuotePersonalDetailsOne />
+      {/* <QuotePersonalDetailsOne /> */}
+      <VechicleDetails />
     </div>
   );
 };
---
+++ src/components/quoteVehicleDetails/VechicleDetails.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const VechicleDetails = () => {
+  return <div>VechicleDetails</div>;
+};
